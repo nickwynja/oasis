99aa73999bb392fdffba9a061b61d932d3288882
Christian Bundy <christianbundy@fraction.io>
2020-02-09 11:28:29 -0800

    Add hacky client plugins for cryptix

--- src/ssb.js
+++ src/ssb.js
@@ -1,5 +1,116 @@
 "use strict";
 
+const flume = require("flumedb");
+const obv = require("obv");
+const path = require("path");
+const pull = require("pull-stream");
+
+let createFakeFilename;
+
+try {
+  const os = require("os");
+  const fs = require("fs");
+
+  createFakeFilename = () => {
+    const result = path.join(
+      fs.mkdtempSync(path.join(os.tmpdir(), "ssb-db-")),
+      "log.flumeproxy"
+    );
+    console.log(`saving view to file ${result}`);
+    return result;
+  };
+} catch (e) {
+  // We're probably running in a browser.
+  createFakeFilename = () => null;
+}
+
+const flumeProxy = remote => {
+  // Create local instance of flumedb that depends on the remote log.
+  // Views will be created locally but the log will remain remote.
+  const since = obv();
+
+  console.log("starting since stream");
+  pull(
+    remote.createSequenceStream(),
+    pull.drain(value => {
+      console.log({ since: value });
+      since.set(value);
+    })
+  );
+
+  const proxy = flume({
+    stream: (opts, cb) => remote.createLogStream({ raw: true, ...opts }, cb),
+    since,
+    get: (seq, cb) => remote.get({ id: seq }, cb),
+    filename: createFakeFilename()
+  });
+
+  const _use = proxy.use;
+
+  let pending = 0;
+  let onReadyCb = null;
+
+  // Match _flumeUse() API from ssb-db
+  proxy._flumeUse = (name, createView) => {
+    pending += 1;
+    _use(name, createView);
+
+    proxy.views[name].ready(() => {
+      console.log(`${name} ready`);
+      pending -= 1;
+      if (pending === 0 && onReadyCb != null) {
+        onReadyCb();
+        onReadyCb = null;
+      }
+    });
+
+    return proxy.views[name];
+  };
+
+  setInterval(() => {
+    Object.entries(proxy.views).forEach(([key, value]) => {
+      console.log(
+        key,
+        `${Math.round((value.since.value / since.value) * 100)}%`
+      );
+    });
+  }, 1000);
+
+  proxy.onReady = cb => {
+    onReadyCb = cb;
+  };
+
+  return proxy;
+};
+
+const magic = ssb => {
+  console.log("magic started");
+  // Use the remote log to create a `use()` function that makes local views.
+  // This means you can create views from ssb-client, not just the server config!
+  const localFlume = flumeProxy(ssb);
+  const _close = ssb.close;
+
+  ssb._flumeUse = localFlume._flumeUse;
+  ssb.close = () => {
+    console.log("api.close() called");
+    localFlume.close(() => {}); // MAGIC NO-OP
+    _close();
+  };
+
+  const self = {
+    use: plugin => {
+      console.log(`installing ${plugin.name}`);
+      if (typeof plugin.init === "function") {
+        ssb[plugin.name] = plugin.init(ssb, { temp: true });
+      }
+      return self;
+    },
+    onReady: localFlume.onReady
+  };
+
+  return self;
+};
+
 // This module exports a function that connects to SSB and returns a "cooler"
 // interface. This interface is poorly defined and should be replaced with
 // native support for Promises in the MuxRPC module.
@@ -7,7 +118,6 @@
 const ssbClient = require("ssb-client");
 const ssbConfig = require("ssb-config");
 const flotilla = require("@fraction/flotilla");
-const ssbTangle = require("ssb-tangle");
 const debug = require("debug")("oasis");
 
 const server = flotilla(ssbConfig);
@@ -25,14 +135,14 @@ const rawConnect = () =>
       if (err) {
         reject(err);
       } else {
-        if (api.tangle === undefined) {
-          // HACK: SSB-Tangle isn't available in Patchwork, but we want that
-          // compatibility. This code automatically injects SSB-Tangle into our
-          // stack so that we don't get weird errors when using Patchwork.
-          //
-          // See: https://github.com/fraction/oasis/issues/21
-          api.tangle = ssbTangle.init(api);
-        }
+        magic(api)
+          .use(require("ssb-backlinks"))
+          .use(require("ssb-about"))
+          // .use(require('ssb-friends')) depends on weird hook that doesn't work over muxrpc
+          .use(require("ssb-logging"))
+          .use(require("ssb-query"))
+          .use(require("ssb-search"))
+          .use(require("ssb-tangle"));
 
         resolve(api);
       }
